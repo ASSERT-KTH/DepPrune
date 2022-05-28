728d2aca26205b732145fa449c1458ae4f5d00c5
刘宇欣 <yuxinliu@n158-p71.eduroam.kth.se>
2022-05-28 16:31:01 +0200

    feat: seperate the two 'removes'

--- removefiles.sh
+++ removefiles.sh
@@ -1,13 +1,7 @@
-jsonFile='.json'
 fileName=$1
 jsonName=$2
 cat $fileName | while read rows
 do
 echo $rows
-if [ ${rows:0-5} = ${jsonFile} ]
-# then rm -rf $rows
-then cat /dev/null > $rows
-else node remove-functions.js $rows $jsonName
-fi
-# 
+cat /dev/null > $rows
 done
