627f52d7b9a98b2abe850f0792dbad0cf32f9740
SandZn <liuyuxin.cpp@icloud.com>
2022-07-25 16:27:03 +0000

    feat: add data and test clients

---
+++ runInstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+variantsNum=`cd Variants/fastify && ls -l |grep "^d"|wc -l`
+
+cd Variants/fastify
+
+echo $variantsNum
+echo `pwd`
+
+for (( i=$variantsNum; i>=1; i--))
+do
+    cd variant$i"/fastify"
+    echo `pwd`
+    echo "npm install variant"$i
+    npm install
+    cd ../..
+done
