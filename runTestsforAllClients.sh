cb945451ef6e0d81344f3b8b97154034c7cc7791
SandZn <liuyuxin.cpp@icloud.com>
2022-08-09 13:58:38 +0000

    feat: add client testing results

--- runTestsforAllClients.sh
+++ runTestsforAllClients.sh
@@ -1,5 +1,11 @@
-# sh runTestClients.sh meow xo 2>>Data/test_meow_xo_func_error.log >> Data/test_meow_xo_func.log
-# sh runTestClients.sh meow stylelint 2>>Data/test_meow_stylelint_func_error.log >> Data/test_meow_stylelint_func.log
-sh runTestClients.sh meow chalk-animation 2>>Data/test_meow_chalk-animation_func_error.log >> Data/test_meow_chalk-animation_func.log
-sh runTestClients.sh meow polyline 2>>Data/test_meow_polyline_func_error.log >> Data/test_meow_polyline_func.log
-sh runTestClients.sh meow tsd 2>>Data/test_meow_tsd_func_error.log >> Data/test_meow_tsd_func.log
+# sh runTestClients.sh deep-equal tape 2>>Data/test_deep-equal_tape_func_error.log >> Data/test_deep-equal_tape_func.log
+# sh runTestClients.sh deep-equal react-mapbox-gl 2>>Data/test_deep-equal_react-mapbox-gl_func_error.log >> Data/test_deep-equal_react-mapbox-gl_func.log
+# sh runTestClients.sh deep-equal http-assert 2>>Data/test_deep-equal_http-assert_func_error.log >> Data/test_deep-equal_http-assert_func.log
+# sh runTestClients.sh deep-equal kbpgp 2>>Data/test_deep-equal_kbpgp_func_error.log >> Data/test_deep-equal_kbpgp_func.log
+# sh runTestClients.sh deep-equal flumeview-reduce 2>>Data/test_deep-equal_flumeview-reduce_func_error.log >> Data/test_deep-equal_flumeview-reduce_func.log
+
+# sh runTestClients.sh deep-equal tape 2>>Data/test_deep-equal_tape_file_error.log >> Data/test_deep-equal_tape_file.log
+sh runTestClients.sh deep-equal react-mapbox-gl 2>>Data/test_deep-equal_react-mapbox-gl_file_error.log >> Data/test_deep-equal_react-mapbox-gl_file.log
+# sh runTestClients.sh deep-equal http-assert 2>>Data/test_deep-equal_http-assert_file_error.log >> Data/test_deep-equal_http-assert_file.log
+# sh runTestClients.sh deep-equal kbpgp 2>>Data/test_deep-equal_kbpgp_file_error.log >> Data/test_deep-equal_kbpgp_file.log
+# sh runTestClients.sh deep-equal flumeview-reduce 2>>Data/test_deep-equal_flumeview-reduce_file_error.log >> Data/test_deep-equal_flumeview-reduce_file.log
