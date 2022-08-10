3e478e1016f5feea53d0642c2e9cb283b2123841
SandZn <liuyuxin.cpp@icloud.com>
2022-08-10 17:06:43 +0000

    feat: add client test result for levelup

--- runInstallClients.sh
+++ runInstallClients.sh
@@ -5,18 +5,20 @@ projectName=$1
 # gitUrls=("https://github.com/expressjs/express.git" "https://github.com/sailshq/skipper.git" "https://github.com/typicode/json-server.git" "https://github.com/strongloop/loopback.git" "https://github.com/typestack/routing-controllers.git")
 
 # send
-clientNames=("serve-static" "restify" "gulp-connect" "fastify-static" "connect-gzip-static")
-gitUrls=("https://github.com/expressjs/serve-static.git" "https://github.com/restify/node-restify.git" "https://github.com/avevlad/gulp-connect.git" "https://github.com/fastify/fastify-static.git" "https://github.com/pirxpilot/connect-gzip-static.git")
+# clientNames=("serve-static" "restify" "gulp-connect" "fastify-static" "connect-gzip-static")
+# gitUrls=("https://github.com/expressjs/serve-static.git" "https://github.com/restify/node-restify.git" "https://github.com/avevlad/gulp-connect.git" "https://github.com/fastify/fastify-static.git" "https://github.com/pirxpilot/connect-gzip-static.git")
 
 # levelup
-# clientNames=("pouchdb" "subleveldown" "packager" "dynalite" "multileveldown")
-# gitUrls=("https://github.com/pouchdb/pouchdb.git" "https://github.com/Level/subleveldown.git" "https://github.com/Level/packager.git" "https://github.com/mhart/dynalite.git" "https://github.com/Level/multileveldown.git")
+clientNames=("pouchdb" "subleveldown" "packager" "dynalite" "multileveldown")
+gitUrls=("https://github.com/pouchdb/pouchdb.git" "https://github.com/Level/subleveldown.git" "https://github.com/Level/packager.git" "https://github.com/mhart/dynalite.git" "https://github.com/Level/multileveldown.git")
 
 # execa
 # clientNames=("yeoman-generator" "lint-staged" "clipboardy" "semantic-release" "yeoman-environment")
 # gitUrls=("https://github.com/yeoman/generator.git" "https://github.com/okonet/lint-staged.git" "https://github.com/sindresorhus/clipboardy.git" "https://github.com/semantic-release/semantic-release.git" "https://github.com/yeoman/environment.git")
 
-
+# serve-index
+# clientNames=("live-server" "server" "fluid-express" "servez-lib")
+# gitUrls=("https://github.com/tapio/live-server.git" "https://github.com/franciscop/server.git" "https://github.com/fluid-project/fluid-express.git" "https://github.com/greggman/servez-lib.git")
 
 for (( i=4; i>=0; i--))
 do
--- runTestsforAllClients.sh
+++ runTestsforAllClients.sh
@@ -1,11 +1,11 @@
-# sh runTestClients.sh send serve-static 2>>Data/test_send_serve-static_func_error.log >> Data/test_send_serve-static_func.log
-# sh runTestClients.sh send node-restify 2>>Data/test_send_node-restify_func_error.log >> Data/test_send_node-restify_func.log
-# sh runTestClients.sh send gulp-connect 2>>Data/test_send_gulp-connect_func_error.log >> Data/test_send_gulp-connect_func.log
-# sh runTestClients.sh send fastify-static 2>>Data/test_send_fastify-static_func_error.log >> Data/test_send_fastify-static_func.log
-# sh runTestClients.sh send connect-gzip-static 2>>Data/test_send_connect-gzip-static_func_error.log >> Data/test_send_connect-gzip-static_func.log
+# sh runTestClients.sh levelup pouchdb 2>>Data/test_levelup_pouchdb_func_error.log >> Data/test_levelup_pouchdb_func.log
+# sh runTestClients.sh levelup subleveldown 2>>Data/test_levelup_subleveldown_func_error.log >> Data/test_levelup_subleveldown_func.log
+# sh runTestClients.sh levelup packager 2>>Data/test_levelup_packager_func_error.log >> Data/test_levelup_packager_func.log
+# sh runTestClients.sh levelup dynalite 2>>Data/test_levelup_dynalite_func_error.log >> Data/test_levelup_dynalite_func.log
+# sh runTestClients.sh levelup multileveldown 2>>Data/test_levelup_multileveldown_func_error.log >> Data/test_levelup_multileveldown_func.log
 
-sh runTestClients.sh send serve-static 2>>Data/test_send_serve-static_file_error.log >> Data/test_send_serve-static_file.log
-sh runTestClients.sh send node-restify 2>>Data/test_send_node-restify_file_error.log >> Data/test_send_node-restify_file.log
-sh runTestClients.sh send gulp-connect 2>>Data/test_send_gulp-connect_file_error.log >> Data/test_send_gulp-connect_file.log
-sh runTestClients.sh send fastify-static 2>>Data/test_send_fastify-static_file_error.log >> Data/test_send_fastify-static_file.log
-sh runTestClients.sh send connect-gzip-static 2>>Data/test_send_connect-gzip-static_file_error.log >> Data/test_send_connect-gzip-static_file.log
+# sh runTestClients.sh levelup pouchdb 2>>Data/test_levelup_pouchdb_file_error.log >> Data/test_levelup_pouchdb_file.log
+# sh runTestClients.sh levelup subleveldown 2>>Data/test_levelup_subleveldown_file_error.log >> Data/test_levelup_subleveldown_file.log
+# sh runTestClients.sh levelup packager 2>>Data/test_levelup_packager_file_error.log >> Data/test_levelup_packager_file.log
+sh runTestClients.sh levelup dynalite 2>>Data/test_levelup_dynalite_file_error.log >> Data/test_levelup_dynalite_file.log
+sh runTestClients.sh levelup multileveldown 2>>Data/test_levelup_multileveldown_file_error.log >> Data/test_levelup_multileveldown_file.log
