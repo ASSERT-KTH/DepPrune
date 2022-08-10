8dc4d9aa0e4a3b6ebf1b7225aa98f369a88eea30
SandZn <liuyuxin.cpp@icloud.com>
2022-08-10 14:16:27 +0000

    feat: add client testing results for send

--- runInstallClients.sh
+++ runInstallClients.sh
@@ -1,16 +1,21 @@
 projectName=$1
 
 # body-parser
-clientNames=("express" "skipper" "json-server" "loopback" "routing-controllers")
-gitUrls=("https://github.com/expressjs/express.git" "https://github.com/sailshq/skipper.git" "https://github.com/typicode/json-server.git" "https://github.com/strongloop/loopback.git" "https://github.com/typestack/routing-controllers.git")
+# clientNames=("express" "skipper" "json-server" "loopback" "routing-controllers")
+# gitUrls=("https://github.com/expressjs/express.git" "https://github.com/sailshq/skipper.git" "https://github.com/typicode/json-server.git" "https://github.com/strongloop/loopback.git" "https://github.com/typestack/routing-controllers.git")
 
 # send
-# clientNames=("serve-static" "restify" "gulp-connect" "fastify-static" "connect-gzip-static")
-# gitUrls=("https://github.com/expressjs/serve-static.git" "https://github.com/restify/node-restify.git" "https://github.com/avevlad/gulp-connect.git" "https://github.com/fastify/fastify-static.git" "https://github.com/pirxpilot/connect-gzip-static.git")
+clientNames=("serve-static" "restify" "gulp-connect" "fastify-static" "connect-gzip-static")
+gitUrls=("https://github.com/expressjs/serve-static.git" "https://github.com/restify/node-restify.git" "https://github.com/avevlad/gulp-connect.git" "https://github.com/fastify/fastify-static.git" "https://github.com/pirxpilot/connect-gzip-static.git")
 
 # levelup
 # clientNames=("pouchdb" "subleveldown" "packager" "dynalite" "multileveldown")
 # gitUrls=("https://github.com/pouchdb/pouchdb.git" "https://github.com/Level/subleveldown.git" "https://github.com/Level/packager.git" "https://github.com/mhart/dynalite.git" "https://github.com/Level/multileveldown.git")
+
+# execa
+# clientNames=("yeoman-generator" "lint-staged" "clipboardy" "semantic-release" "yeoman-environment")
+# gitUrls=("https://github.com/yeoman/generator.git" "https://github.com/okonet/lint-staged.git" "https://github.com/sindresorhus/clipboardy.git" "https://github.com/semantic-release/semantic-release.git" "https://github.com/yeoman/environment.git")
+
 
 
 for (( i=4; i>=0; i--))
--- runTestsforAllClients.sh
+++ runTestsforAllClients.sh
@@ -1,11 +1,11 @@
-# sh runTestClients.sh body-parser express 2>>Data/test_body-parser_express_func_error.log >> Data/test_body-parser_express_func.log
-# sh runTestClients.sh body-parser skipper 2>>Data/test_body-parser_skipper_func_error.log >> Data/test_body-parser_skipper_func.log
-# sh runTestClients.sh body-parser json-server 2>>Data/test_body-parser_json-server_func_error.log >> Data/test_body-parser_json-server_func.log
-# sh runTestClients.sh body-parser loopback 2>>Data/test_body-parser_loopback_func_error.log >> Data/test_body-parser_loopback_func.log
-# sh runTestClients.sh body-parser routing-controllers 2>>Data/test_body-parser_routing-controllers_func_error.log >> Data/test_body-parser_routing-controllers_func.log
+# sh runTestClients.sh send serve-static 2>>Data/test_send_serve-static_func_error.log >> Data/test_send_serve-static_func.log
+# sh runTestClients.sh send node-restify 2>>Data/test_send_node-restify_func_error.log >> Data/test_send_node-restify_func.log
+# sh runTestClients.sh send gulp-connect 2>>Data/test_send_gulp-connect_func_error.log >> Data/test_send_gulp-connect_func.log
+# sh runTestClients.sh send fastify-static 2>>Data/test_send_fastify-static_func_error.log >> Data/test_send_fastify-static_func.log
+# sh runTestClients.sh send connect-gzip-static 2>>Data/test_send_connect-gzip-static_func_error.log >> Data/test_send_connect-gzip-static_func.log
 
-# sh runTestClients.sh body-parser express 2>>Data/test_body-parser_express_file_error.log >> Data/test_body-parser_express_file.log
-# sh runTestClients.sh body-parser skipper 2>>Data/test_body-parser_skipper_file_error.log >> Data/test_body-parser_skipper_file.log
-sh runTestClients.sh body-parser json-server 2>>Data/test_body-parser_json-server_file_error.log >> Data/test_body-parser_json-server_file.log
-# sh runTestClients.sh body-parser loopback 2>>Data/test_body-parser_loopback_file_error.log >> Data/test_body-parser_loopback_file.log
-# sh runTestClients.sh body-parser routing-controllers 2>>Data/test_body-parser_routing-controllers_file_error.log >> Data/test_body-parser_routing-controllers_file.log
+sh runTestClients.sh send serve-static 2>>Data/test_send_serve-static_file_error.log >> Data/test_send_serve-static_file.log
+sh runTestClients.sh send node-restify 2>>Data/test_send_node-restify_file_error.log >> Data/test_send_node-restify_file.log
+sh runTestClients.sh send gulp-connect 2>>Data/test_send_gulp-connect_file_error.log >> Data/test_send_gulp-connect_file.log
+sh runTestClients.sh send fastify-static 2>>Data/test_send_fastify-static_file_error.log >> Data/test_send_fastify-static_file.log
+sh runTestClients.sh send connect-gzip-static 2>>Data/test_send_connect-gzip-static_file_error.log >> Data/test_send_connect-gzip-static_file.log
