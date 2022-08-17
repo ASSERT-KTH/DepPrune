bfe26c015f13d30fbec45c53eea2d35e3f2319d2
SandZn <liuyuxin.cpp@icloud.com>
2022-08-17 17:02:43 +0200

    feat: log signal into error log

--- runTestsforAllClients.sh
+++ runTestsforAllClients.sh
@@ -1,6 +1,6 @@
-sh runTestClients.sh fastify fastify-micro 2>>Data/test_fastify_fastify-micro_func_error.log >> Data/test_fastify_fastify-micro_func.log
-sh runTestClients.sh fastify fastify-helpers 2>>Data/test_fastify_fastify-helpers_func_error.log >> Data/test_fastify_fastify-helpers_func.log
-sh runTestClients.sh fastify tydb 2>>Data/test_fastify_tydb_func_error.log >> Data/test_fastify_tydb_func.log
+# sh runTestClients.sh fastify fastify-micro 2>>Data/test_fastify_fastify-micro_func_error.log >> Data/test_fastify_fastify-micro_func.log
+# sh runTestClients.sh fastify fastify-helpers 2>>Data/test_fastify_fastify-helpers_func_error.log >> Data/test_fastify_fastify-helpers_func.log
+# sh runTestClients.sh fastify tydb 2>>Data/test_fastify_tydb_func_error.log >> Data/test_fastify_tydb_func.log
 sh runTestClients.sh fastify fastify-vhost 2>>Data/test_fastify_fastify-vhost_func_error.log >> Data/test_fastify_fastify-vhost_func.log
 sh runTestClients.sh fastify pubsub-http-handler 2>>Data/test_fastify_pubsub-http-handler_func_error.log >> Data/test_fastify_pubsub-http-handler_func.log
 
