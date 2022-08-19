1cb3991178d6e422e6a7f8a920cfde5b80540270
SandZn <liuyuxin.cpp@icloud.com>
2022-08-19 09:56:22 +0000

    fix: log error from overwrite to append

--- runTestClients.sh
+++ runTestClients.sh
@@ -5,7 +5,7 @@ clientName=$2
 # echo $(pwd)
 
 # echo $variantsNum
-# echo $variantsNum > /dev/stderr
+# echo $variantsNum >> /dev/stderr
 
 # # For each variant, we test its client under the corresponding dependency
 # # 1. Unlink the module(variant)'s name
@@ -17,49 +17,49 @@ clientName=$2
 # for (( i=$variantsNum; i>=1; i--))
 # do
 #     echo $clientName" is testing ./Variants/"$projectName"/variant"$i"/"$projectName
-#     echo $clientName" is testing ./Variants/"$projectName"/variant"$i"/"$projectName > /dev/stderr
+#     echo $clientName" is testing ./Variants/"$projectName"/variant"$i"/"$projectName >> /dev/stderr
 #     echo $(pwd)
 #     cd "Clients/"$projectName"/"$clientName
 
 #     # step 1
 #     echo "************* npm unlink module *************"
-#     echo "************* npm unlink module *************" > /dev/stderr
+#     echo "************* npm unlink module *************" >> /dev/stderr
 
 #     npm unlink --no-save $projectName
 #     echo "************* npm unlink module done *************"
-#     echo "************* npm unlink module done *************" > /dev/stderr
+#     echo "************* npm unlink module done *************" >> /dev/stderr
 
 #     # step 2
 #     echo "************* npm unlink globally *************"
-#     echo "************* npm unlink globally *************" > /dev/stderr
+#     echo "************* npm unlink globally *************" >> /dev/stderr
 #     sudo npm rm --global $projectName
 #     echo "************* npm unlink globally done *************"
-#     echo "************* npm unlink globally done *************" > /dev/stderr
+#     echo "************* npm unlink globally done *************" >> /dev/stderr
 
 #     # step 3
 #     cd "../../../Variants/"$projectName"/variant"$i"/"$projectName
 #     echo "************* npm link globally *************"
-#     echo "************* npm link globally *************" > /dev/stderr
+#     echo "************* npm link globally *************" >> /dev/stderr
 #     sudo npm link
 #     echo "************* npm link globally done *************"
-#     echo "************* npm link globally done *************" > /dev/stderr
+#     echo "************* npm link globally done *************" >> /dev/stderr
 
 #     # step 4
 #     cd "../../../../Clients/"$projectName"/"$clientName
 #     echo "************* npm link module *************"
-#     echo "************* npm link module *************" > /dev/stderr
+#     echo "************* npm link module *************" >> /dev/stderr
 #     npm link $projectName
 #     echo "************* npm link module done *************"
-#     echo "************* npm link module done *************" > /dev/stderr
+#     echo "************* npm link module done *************" >> /dev/stderr
 
 #     # step 5
 #     echo "************* start run test *************"
-#     echo "************* start run test *************" > /dev/stderr
+#     echo "************* start run test *************" >> /dev/stderr
 #     # timeout -k 30s 2m npm run test
 #     timeout -k 300s 2m npm run test
 #     cd ../../..
 #     echo "************* run test done *************"
-#     echo "************* run test done *************" > /dev/stderr
+#     echo "************* run test done *************" >> /dev/stderr
 # done
 
 variantsDepsNum=`cd VariantsDeps/$projectName && ls -l |grep "^d"|wc -l`
@@ -67,52 +67,52 @@ echo "variantsDepsNum: "$variantsDepsNum
 echo $(pwd)
 
 echo $variantsDepsNum
-echo $variantsDepsNum > /dev/stderr
+echo $variantsDepsNum >> /dev/stderr
 
 
 for (( i=$variantsDepsNum; i>=1; i--))
 do
     echo $clientName" is testing ./VariantsDeps/"$projectName"/variant"$i"/"$projectName
-    echo $clientName" is testing ./VariantsDeps/"$projectName"/variant"$i"/"$projectName > /dev/stderr
+    echo $clientName" is testing ./VariantsDeps/"$projectName"/variant"$i"/"$projectName >> /dev/stderr
     echo $(pwd)
-    echo $(pwd) > /dev/stderr
+    echo $(pwd) >> /dev/stderr
     
     cd "Clients/"$projectName"/"$clientName
     # step 1
     echo "************* npm unlink module *************"
-    echo "************* npm unlink module *************" > /dev/stderr
+    echo "************* npm unlink module *************" >> /dev/stderr
     npm unlink --no-save $projectName
     echo "************* npm unlink module done *************"
-    echo "************* npm unlink module done *************" > /dev/stderr
+    echo "************* npm unlink module done *************" >> /dev/stderr
 
     # step 2
     echo "************* npm unlink globally *************"
-    echo "************* npm unlink globally *************" > /dev/stderr
+    echo "************* npm unlink globally *************" >> /dev/stderr
     sudo npm rm --global $projectName
     echo "************* npm unlink module done *************"
-    echo "************* npm unlink module done *************" > /dev/stderr
+    echo "************* npm unlink module done *************" >> /dev/stderr
 
     # step 3
     cd "../../../VariantsDeps/"$projectName"/variant"$i"/"$projectName
     echo "************* npm link globally *************"
-    echo "************* npm link globally *************" > /dev/stderr
+    echo "************* npm link globally *************" >> /dev/stderr
     sudo npm link
     echo "************* npm link globally done *************"
-    echo "************* npm link globally done *************" > /dev/stderr
+    echo "************* npm link globally done *************" >> /dev/stderr
 
     # step 4
     cd "../../../../Clients/"$projectName"/"$clientName
     echo "************* npm link module *************"
-    echo "************* npm link module *************" > /dev/stderr
+    echo "************* npm link module *************" >> /dev/stderr
     npm link $projectName
     echo "************* npm link module done *************"
-    echo "************* npm link module done *************" > /dev/stderr
+    echo "************* npm link module done *************" >> /dev/stderr
 
     echo "************* start run test *************"
-    echo "************* start run test *************" > /dev/stderr
+    echo "************* start run test *************" >> /dev/stderr
     # timeout -k 30s 2m npm run test
     timeout -k 300s 2m npm run test
     cd ../../..
     echo "************* run test done *************"
-    echo "************* run test done *************" > /dev/stderr
+    echo "************* run test done *************" >> /dev/stderr
 done
--- runTestsforAllClients.sh
+++ runTestsforAllClients.sh
@@ -1,11 +1,15 @@
 # sh runTestClients.sh fastify fastify-micro 2>>Data/test_fastify_fastify-micro_func_error.log >> Data/test_fastify_fastify-micro_func.log
 # sh runTestClients.sh fastify fastify-helpers 2>>Data/test_fastify_fastify-helpers_func_error.log >> Data/test_fastify_fastify-helpers_func.log
 # sh runTestClients.sh fastify tydb 2>>Data/test_fastify_tydb_func_error.log >> Data/test_fastify_tydb_func.log
-sh runTestClients.sh fastify fastify-vhost 2>>Data/test_fastify_fastify-vhost_func_error.log >> Data/test_fastify_fastify-vhost_func.log
-sh runTestClients.sh fastify pubsub-http-handler 2>>Data/test_fastify_pubsub-http-handler_func_error.log >> Data/test_fastify_pubsub-http-handler_func.log
+# sh runTestClients.sh fastify fastify-vhost 2>>Data/test_fastify_fastify-vhost_func_error.log >> Data/test_fastify_fastify-vhost_func.log
+# sh runTestClients.sh fastify pubsub-http-handler 2>>Data/test_fastify_pubsub-http-handler_func_error.log >> Data/test_fastify_pubsub-http-handler_func.log
 
 # sh runTestClients.sh fastify fastify-micro 2>>Data/test_fastify_fastify-micro_file_error.log >> Data/test_fastify_fastify-micro_file.log
 # sh runTestClients.sh fastify fastify-helpers 2>>Data/test_fastify_fastify-helpers_file_error.log >> Data/test_fastify_fastify-helpers_file.log
 # sh runTestClients.sh fastify tydb 2>>Data/test_fastify_tydb_file_error.log >> Data/test_fastify_tydb_file.log
 # sh runTestClients.sh fastify fastify-vhost 2>>Data/test_fastify_fastify-vhost_file_error.log >> Data/test_fastify_fastify-vhost_file.log
 # sh runTestClients.sh fastify pubsub-http-handler 2>>Data/test_fastify_pubsub-http-handler_file_error.log >> Data/test_fastify_pubsub-http-handler_file.log
+
+sh runTestClients.sh deep-equal react-mapbox-gl 2>>Data/test_error.log >> Data/test_file.log
+# sh runTestClients.sh deep-equal react-mapbox-gl >> Data/test_fastify_react-mapbox-gl_file.log 2>&1 
+# sh runTestClients.sh deep-equal kbpgp 2>>Data/test_fastify_kbpgp_file_error.log >> Data/test_fastify_kbpgp_file.log
