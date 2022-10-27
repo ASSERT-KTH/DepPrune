97dbd2166caf43a1d70ad8fe72f9bf1ce23453d5
SandZn <liuyuxin.cpp@icloud.com>
2022-10-27 14:03:46 +0000

    feat: try Madge

--- tryMadge.js
+++ tryMadge.js
@@ -1,9 +1,66 @@
 const madge = require('madge');
 
-// madge('Playground/express/index.js').then((res) => {
-//     console.log(res.obj());
-// })
-
-madge('Playground/express/index.js').then((res) => {
+madge('Playground/body-parser/index.js').then((res) => {
+    console.log("body-parser")
     console.log(res.obj());
 })
+madge('Playground/compression/index.js').then((res) => {
+    console.log("compression")
+    console.log(res.obj());
+})
+madge('Playground/deep-equal/index.js').then((res) => {
+    console.log("deep-equal")
+    console.log(res.obj());
+})
+madge('Playground/execa/index.js').then((res) => {
+    console.log("execa")
+    console.log(res.obj());
+})
+madge('Playground/express/index.js').then((res) => {
+    console.log("express")
+    console.log(res.obj());
+})
+madge('Playground/fastify/fastify.js').then((res) => {
+    console.log("fastify")
+    console.log(res.obj());
+})
+madge('Playground/finalhandler/index.js').then((res) => {
+    console.log("finalhandler")
+    console.log(res.obj());
+})
+madge('Playground/levelup/lib/levelup.js').then((res) => {
+    console.log("levelup")
+    console.log(res.obj());
+})
+madge('Playground/memdown/memdown.js').then((res) => {
+    console.log("memdown")
+    console.log(res.obj());
+})
+madge('Playground/meow/index.js').then((res) => {
+    console.log("meow")
+    console.log(res.obj());
+})
+madge('Playground/send/index.js').then((res) => {
+    console.log("body-parser")
+    console.log(res.obj());
+})
+madge('Playground/serve-index/index.js').then((res) => {
+    console.log("serve-index")
+    console.log(res.obj());
+})
+madge('Playground/session/index.js').then((res) => {
+    console.log("session")
+    console.log(res.obj());
+})
+madge('Playground/sharp/lib/index.js').then((res) => {
+    console.log("sharp")
+    console.log(res.obj());
+})
+madge('Playground/yeoman-generator/lib/index.js').then((res) => {
+    console.log("yeoman-generator")
+    console.log(res.obj());
+})
+
+// madge('Playground/body-parser/index.js').then((res) => {
+//     console.log(res.obj());
+// })
