82bb7870639e6b4f0abe5d1d0d10f85335461a62
SandZn <liuyuxin.cpp@icloud.com>
2023-01-27 09:03:02 +0000

    feat:add projects

--- txtToExcel.py
+++ txtToExcel.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-dataframe = pd.read_csv('top_dependencies_temp.txt')
-dataframe.to_csv('top_dependencies_temp.csv', index = None)
+dataframe = pd.read_csv('top_target30.txt')
+dataframe.to_csv('top_target30.csv', index = None)
