83a7744d9d9f48e7f82c5a562ef411e02cf5c6cf
SandZn <liuyuxin.cpp@icloud.com>
2023-02-03 11:44:54 +0000

    feat: add data

--- txtToExcel.py
+++ txtToExcel.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-dataframe = pd.read_csv('top_target30.txt')
-dataframe.to_csv('top_target30.csv', index = None)
+dataframe = pd.read_csv('top_491_with_deps_5_90.txt')
+dataframe.to_csv('top_491_with_deps_5_90.csv', index = None)
