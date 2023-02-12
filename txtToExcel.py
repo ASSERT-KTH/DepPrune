eaf11f6774a18fcd83c3ba99b98ca525204ded53
SandZn <liuyuxin.cpp@icloud.com>
2023-02-12 17:27:29 +0000

    feat: generate csv files

--- txtToExcel.py
+++ txtToExcel.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-dataframe = pd.read_csv('top_dependencies_5_92.txt')
-dataframe.to_csv('top_dependencies_5_92.csv', index = None)
+dataframe = pd.read_csv('top_total_file_number.txt')
+dataframe.to_csv('top_total_file_number.csv', index = None)
