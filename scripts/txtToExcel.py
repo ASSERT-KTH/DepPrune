4998eb1538cdb9720238e76ca591219eb003dc01
SandZn <liuyuxin.cpp@icloud.com>
2023-06-11 11:38:02 +0000

    feat: add valid readme

--- scripts/txtToExcel.py
+++ scripts/txtToExcel.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-dataframe = pd.read_csv('./Logs/repo_NodeJS_100000_commits.txt')
-dataframe.to_csv('./Csvs/repo_NodeJS_100000_commits.csv', index = None)
+dataframe = pd.read_csv('./Logs/repo_100000_readme.txt')
+dataframe.to_csv('./Csvs/repo_100000_readme.csv', index = None)
