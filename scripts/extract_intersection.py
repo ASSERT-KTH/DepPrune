c5e27081e6218f94983882b0e2c111fa3a1ae5f9
SandZn <liuyuxin.cpp@icloud.com>
2023-06-11 13:38:14 +0000

    feat: add data

--- scripts/extract_intersection.py
+++ scripts/extract_intersection.py
@@ -2,25 +2,28 @@ filePath1 = f'./Logs/repo_100000_readme.txt'
 with open(filePath1) as f:
     lines1 = f.read().splitlines()
 
-filePath2 = f'./Logs/repo_100000_readme_error.txt'
-with open(filePath2) as f:
-    lines2 = f.read().splitlines()
+# filePath2 = f'./Logs/repo_100000_readme_error.txt'
+# with open(filePath2) as f:
+#     lines2 = f.read().splitlines()
 
 list1 = []
 for item in lines1:
     arr = item.split(',')
+    git_url = f'https://github.com/{arr[1]}.git'
+    arr.pop(6)
+    arr.append(git_url)
     list1.append(arr)
 
-list2 = []
-for item in lines2:
-    arr = item.split(',')
-    list2.append(arr)
+# list2 = []
+# for item in lines2:
+#     arr = item.split(',')
+#     list2.append(arr)
 
-intersection = [arr2 for arr1 in list1 for arr2 in list2 if arr1[1] == arr2[0]]
+# intersection = [arr2 for arr1 in list1 for arr2 in list2 if arr1[1] == arr2[0]]
 
 
-collection_file = open("./Logs/repo_100000_readme_error_rest.txt", "a")
-for item in list2:
+collection_file = open("./Logs/repo_100000_readme_github.txt", "a")
+for item in list1:
     line = ",".join(item) + "\n"
     collection_file.writelines(line)
     
--- scripts/txtToExcel.py
+++ scripts/txtToExcel.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-dataframe = pd.read_csv('./Logs/repo_100000_readme.txt')
-dataframe.to_csv('./Csvs/repo_100000_readme.csv', index = None)
+dataframe = pd.read_csv('./Logs/repo_100000_readme_github.txt')
+dataframe.to_csv('./Csvs/repo_100000_readme_github.csv', index = None)
