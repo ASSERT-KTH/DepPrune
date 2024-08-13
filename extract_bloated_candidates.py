e497e0bedb00158fdbddc2482529de5be74497df
SandZn <liuyuxin.cpp@icloud.com>
2024-08-13 10:16:32 +0000

    feat: update scripts

--- extract_bloated_candidates.py
+++ extract_bloated_candidates.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import json
 
 def get_run_deps(dep_path):
     runtime_deps = []
@@ -28,7 +29,18 @@ def collect_files(dependencies, files):
         "reachable_files": collected_files,
         "reachable_deps": reachable_deps
     }
-
+def extract_direct_deps(pkg_json_path):
+    try:
+        with open(pkg_json_path, 'r') as file:
+            package_data = json.load(file)
+        
+        dependencies = package_data.get('dependencies', {})
+        dependency_names = list(dependencies.keys())
+        return dependency_names
+        
+    except FileNotFoundError:
+        print(f"File not found: {pkg_json_path}")
+        return []
 
 if __name__ == "__main__":
     project = sys.argv[1]
@@ -43,6 +55,8 @@ if __name__ == "__main__":
 
     result = collect_files(deps, opened_files)
 
+    pkg_json_path = f'./{folder}/{project}/package.json'
+
     output_path = f'./{folder}/{project}/opened_runtime_files.txt'
     output_file = open(output_path, "a")
     for item in result["reachable_files"]:
@@ -55,11 +69,33 @@ if __name__ == "__main__":
     for item in reachable:
         # print(item)
         output_dep_file.writelines(item + '\n')
-
-    output_udep_path = f'./{folder}/{project}/unreachable_runtime_deps_os.txt'
-    output_udep_file = open(output_udep_path, "a")
+    
     unreachable = list(set(deps) - set(reachable))
     print("unreachable deps", unreachable)
+
+    output_all_path = f'./{folder}/{project}/unreachable_os_all.txt'
+    output_all_file = open(output_all_path, "a")
     for item in unreachable:
         # print(item)
-        output_udep_file.writelines(item + '\n')
+        output_all_file.writelines(item + '\n')
+
+    output_direct_path = f'./{folder}/{project}/unreachable_os_direct.txt'
+    output_direct_file = open(output_direct_path, "a")
+    output_indirect_path = f'./{folder}/{project}/unreachable_os_indirect.txt'
+    output_indirect_file = open(output_indirect_path, "a")
+    
+    
+    # seperate the deps by direct and indirect
+    direct_names = extract_direct_deps(pkg_json_path)
+    for dep_name in direct_names:
+        direct_folder = 'node_modules/' + dep_name
+        if direct_folder in unreachable:
+            print("unreachable direct: ", dep_name)
+            output_direct_file.writelines(direct_folder + '\n')
+            unreachable.remove(direct_folder)
+    
+    for item in unreachable:
+        output_indirect_file.writelines(item + '\n')
+            
+
+
